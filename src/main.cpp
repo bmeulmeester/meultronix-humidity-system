33a3dd5593cfd7053c6eb15fe442d22b1bf07024
Bryan Sam Meulmeester <bryanmeulmeester@gmail.com>
2020-06-09 15:37:02 +0200

    Performed some tests using the new abstraction.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#include <MTAccessPoint.h>
+#include <MTLedPin.h>
+
+LedPin *builtInLedPin;
+MTAccessPoint *accessPoint;
+
+void setup()
+{
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+
+  builtInLedPin = new LedPin(LED_BUILTIN);
+
+  accessPoint = new MTAccessPoint("Meultronix", "BSM1992!", 80);
+  accessPoint->startBroadcasting();
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+  builtInLedPin->setEnabled(true);
+  delay(1000);
+  builtInLedPin->setEnabled(false);
+  delay(1000);
 }
